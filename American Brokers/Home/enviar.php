91be3a8078b92333ed43487bdd98139cf13f93eb
andres25ortega <andres25ortega@gmail.com>
2020-08-22 21:39:30 -0500

    Formulario e iconos flotantes

---
+++ American Brokers/Home/enviar.php
@@ -0,0 +1,27 @@
+
+<?php
+
+//En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
+ $destino = "andres25ortega@gmail.com";
+
+    $nombre = $_POST["nombre"];
+
+    $IndPais = $_POST["IndPais"];
+
+    $IndCiudad = $_POST["IndCiudad"];
+
+    $telefono = $_POST["telefono"];
+
+    $correo = $_POST["correo"];
+
+    $ciudad = $_POST["ciudad"];
+
+    $mensaje = $_POST["mensaje"];
+
+    $contenido = "Nombre: " . $nombre . "\nInd. País: " . $IndPais . "\nInd. Ciudad: " . $IndCiudad . "\nTelefono: " . $telefono . "\nCiudad: " . $ciudad . "\nCorreo: " . $correo . "\nAsunto: " . $mensaje;
+
+    mail($destino, "Contacto", $contenido);
+
+    // header("Location: A_Donde_Lo_Quieras_Redireccionar");
+//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+?>
--- American Brokers/assets/js/main.js
+++ American Brokers/assets/js/main.js
@@ -1,18 +1,18 @@
 AOS.init();
-// let irArriba = document.getElementById('irArriba');
-// let imgIrArriba = document.getElementById('imgIrArriba');
+let irArriba = document.getElementById('irArriba');
+let imgIrArriba = document.getElementById('imgIrArriba');
 
-// window.onscroll = function () {
-//     if (document.documentElement.scrollTop > 200) {
-//         irArriba.classList.remove('d-none')
-//     }else{
-//         irArriba.classList.add('d-none')
-//     }
-// }
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        irArriba.classList.remove('d-none')
+    }else{
+        irArriba.classList.add('d-none')
+    }
+}
 
-// imgIrArriba.addEventListener('click', () => {
-//     window.scrollTo({
-//         top:0,
-//         behavior: "smooth"
-//     })
-// })
+imgIrArriba.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+})
